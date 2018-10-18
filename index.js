a21eb56939059c37fe89254f391380a6f5581abf
samuelomotayo <samuelomotay@gmail.com>
2018-10-18 20:22:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  const hq =42;
+  if (block < hq) {
+    return hq - block;
+  }
+  return block - hq;
+}
+ 
+function distanceFromHqInFeet(){
+  
+}
+
+  
+
+
+
+
+
+
+
+function scuberGreetingForFeet(distance){
+   if (distance >=2001){
+     return 'I will gladly take your thirty bucks!';
+   }
+ }
+ 
